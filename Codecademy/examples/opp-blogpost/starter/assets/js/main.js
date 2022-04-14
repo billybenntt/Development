4739fe7e186de6a41cb8b15c18a2de078d8f7ff9
billbenntt@gapp.nthu.edu.tw <billbenntt@gapp.nthu.edu.tw>
2022-04-14 17:57:55 +0800

    personal project update 4-14-22-1

--- Codecademy/examples/opp-blogpost/starter/assets/js/main.js
+++ Codecademy/examples/opp-blogpost/starter/assets/js/main.js
@@ -1,6 +1,29 @@
 // Grab elements
 
+//  Check that element exist function
+const selectElement = (selector) => {
+  // Store the value whatever we pass in
+  const element = document.querySelector(selector);
+  // If Element Exists Return ELement
+  if (element) return element;
+  // Else Throw an error
+  throw new Error(`Something went wrong, make sure ${selector} exists`);
+};
+
 //Nav styles on scroll
+
+const scrollHeader = () => {
+  // Select Heaader
+  const headerElement = selectElement("#header");
+  if (this.scrollY >= 15) {
+    headerElement.classList.add("activated");
+  } else {
+    headerElement.classList.remove("activated");
+  }
+};
+
+// Define function Trigger
+window.addEventListener("scroll", scrollHeader);
 
 // Open menu & search pop-up
 
