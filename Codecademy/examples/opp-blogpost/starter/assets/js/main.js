157c29abf8583b91013743f02eb826760e2add05
billbenntt@gapp.nthu.edu.tw <billbenntt@gapp.nthu.edu.tw>
2022-04-19 17:06:24 +0800

    personal project update 4-14-22-1

--- Codecademy/examples/opp-blogpost/starter/assets/js/main.js
+++ Codecademy/examples/opp-blogpost/starter/assets/js/main.js
@@ -79,3 +79,25 @@ themeToggleBtn.addEventListener("click", () => {
 });
 
 // Swiper
+
+const swiper = new Swiper(".swiper", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevtEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  breakpoints: {
+    700: {
+      slidesPerView: 2,
+    },
+
+    1200: {
+      slidesPerView: 3,
+    },
+  },
+});
