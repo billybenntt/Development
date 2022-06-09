e336bd17e62c461f065b06a7818a27a7ecbe3070
billbenntt@gapp.nthu.edu.tw <billbenntt@gapp.nthu.edu.tw>
2022-06-09 08:03:29 +0800

    update

--- Kotlin Development/basics-kotlin/src/main/kotlin/Main.kt
+++ Kotlin Development/basics-kotlin/src/main/kotlin/Main.kt
@@ -1,28 +1,6 @@
 fun main() {
 
-// Shopping List String
-    val shoppingList = mutableListOf(
-        "Cherry Pomme",
-        "Apple",
-        "Apricots",
-        "Avocado",
-        "Banana",
-        "Blackberries",
-        "Blueberries",
-        "Breadfruit",
-        "Cantaloupe",
-        "Cherries",
-    )
-
-    // Iterator Function
-    shoppingList.forEach {
-        println(it)
-    }
-
-    // Iterator Function without IT
-    shoppingList.forEach { interestingFruits ->
-        println(interestingFruits)
-    }
+    println("Test")
 
 
 }
