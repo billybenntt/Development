042d0275d65f6ee25ec4ffcc6e96984a5492a137
billbenntt@gapp.nthu.edu.tw <billbenntt@gapp.nthu.edu.tw>
2022-07-13 16:57:57 +0800

    update

---
+++ Javascript/Javascript-FETCH-2/fetch-2.js
@@ -0,0 +1,37 @@
+// Target API
+const url = "https://reqres.in/api/users";
+
+// Argument Object
+const initObject = {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+        name: 'User 1'
+    })
+}
+
+
+
+
+// Step 2 - Convert Response to JSON and Return a Promise
+const convertToJson = (response) => {
+    if (response.ok) {
+        return response.json()
+    }
+    throw new Error("Not Found")
+}
+
+// Step 3 - Log Response
+const logResponse = (response) => {
+    console.log(response)
+}
+
+// Step 4 - Handle Error
+const logError = (reason) => {
+    console.log(reason)
+}
+
+fetch(url, initObject).then(convertToJson).then(logResponse).catch(logError)
+
