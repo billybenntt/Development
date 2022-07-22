823cb0a21ce017284d4a735bee926f88152cdb87
billbenntt@gapp.nthu.edu.tw <billbenntt@gapp.nthu.edu.tw>
2022-07-22 17:18:31 +0800

    update react

---
+++ React/src/App.js
@@ -0,0 +1,20 @@
+import Page from './components/header'
+import React from 'react';
+
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="mainCless">
+
+        <Page />
+
+      </div>
+    )
+  }
+}
+
+
+
+export default App;
