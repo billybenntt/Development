ea15002f5738563b68bf104cd7e94b60988eacad
billbenntt@gapp.nthu.edu.tw <billbenntt@gapp.nthu.edu.tw>
2022-07-28 17:29:08 +0800

    update react

--- React/react-basics-2/src/App.jsx
+++ React/react-basics-2/src/App.jsx
@@ -6,10 +6,13 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-
       <Hero />
+      <div className="subcontainer">
+        <Card name="juas" price="1200" />
+        <Card name="gringa" price="2020" />
+      </div>
 
-      <Card />
+
     </div>
   )
 }
--- React/react-basics-2/src/components/Card.jsx
+++ React/react-basics-2/src/components/Card.jsx
@@ -2,17 +2,20 @@ import React from 'react'
 import pictureCard from "../assets/katie-zaferes.png"
 import pictureStar from "../assets/star.png"
 
-function Card() {
+function Card(props) {
+
+    const { name, price } = props;
+
 
     return (
-        <div className="container">
+        <div className="card--container">
             <div className="card">
                 <img src={pictureCard} alt="picture" className='card--image' />
                 <div className="card--info">
                     <img src={pictureStar} alt="picture" className='card--star' />
                     <span className='card--rating'>5.0 (6).USA</span>
-                    <p className='card--title'>Life Lessons with Katie Saferes</p>
-                    <span className='card--price'>From $136 / Person</span>
+                    <p className='card--title'>Life {name} with {name} Saferes</p>
+                    <span className='card--price'>From ${price} / Person</span>
                 </div>
 
             </div>
