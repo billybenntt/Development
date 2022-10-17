f2d6fa8618a2c8355ff42670aef42e66618d4e70
billbenntt@gapp.nthu.edu.tw <billbenntt@gapp.nthu.edu.tw>
2022-10-17 17:47:03 +0800

    update

--- VANILLA/vanilla-project-14-groceryList/app.js
+++ VANILLA/vanilla-project-14-groceryList/app.js
@@ -1,57 +1,93 @@
 // --------------- SELECT ITEMS ---------------
-const alertElement = document.querySelector(".alert");
-const form = document.querySelector(".grocery-form");
-const groceryAdd = document.getElementById("grocery");
-const submitBtn = document.querySelector(".submit-btn");
-const groceryContainer = document.querySelector(".grocery-container");
-const groceryList = document.querySelector(".grocery-list");
-const clearBtn = document.querySelector(".clear-btn");
+const alertElement = document.querySelector('.alert')
+/* Form Container*/
+const form = document.querySelector('.grocery-form')
+const groceryAdd = document.getElementById('grocery')
+const submitBtn = document.querySelector('.submit-btn')
+const groceryContainer = document.querySelector('.grocery-container')
+
+/*Target Container*/
+const groceryList = document.querySelector('.grocery-list')
+const clearBtn = document.querySelector('.clear-btn')
 
 // --------------- EDIT OPTIONS---------------
-let editElement;
-let editFlag = false;
-let editID = '';
-
+let editElement
+let editFlag = false
+let editID = ''
 
 // --------------- FUNCTIONS FOR EVENT LISTENER ---------------
 const addItem = (event) => {
-    //Prevent Refresh
-    event.preventDefault();
+  //Prevent Refresh
+  event.preventDefault()
 
-    // Capture Form Input
-    const value = groceryAdd.value
-    const id = new Date().getTime().toString()
+  // Capture Form Input
+  const value = groceryAdd.value
+  const id = new Date().getTime().toString()
 
-    if (value && !editFlag) {
-        console.log("Adding New")
+  // Adding New Item
+  if (value && !editFlag) {
+    // Create new element
+    const element = document.createElement('article')
+    //  Add Class
+    element.classList.add('grocery-item')
+    // Add ID Attribute
+    const attr = document.createAttribute('data-id')
+    // Assign Unique value
+    attr.value = id
+    element.setAttributeNode(attr)
+    // Fill the Element generic content
+    element.innerHTML = `<p class="title">${value}</p>
+                    <div class="btn-container">
+                        <button type="button" class="edit-btn">
+                            <i class="fas fa-edit"></i>
+                        </button>
+                        <button type="button" class="delete-btn">
+                            <i class="fas fa-trash"></i>
+                        </button>
+                     </div>`
+    // Append Child
+    groceryList.appendChild(element)
+    //  Display alert
+    displayAlert('Item Added to the list', 'success')
+    //  Show Container
+    groceryContainer.classList.add('show-container')
+    // Add to Local Storage
+    addToLocalStorage(id, value)
+    // Set Back to Default
+    setBackToDefault(id, value)
 
+  } else if (value && editFlag) {
+    console.log('Editing')
 
-    } else if (value && editFlag) {
-        console.log("Editing")
-
-
-    } else {
-        displayAlert("Please enter value", "danger")
-    }
-
+  } else {
+    displayAlert('Please enter value', 'danger')
+  }
 
 }
 
 const displayAlert = (text, action) => {
-    alertElement.textContent = text;
-    alertElement.classList.add(`alert-${action}`)
-
-    setTimeout(() => {
-        alertElement.textContent = "";
-        alertElement.classList.add(`alert-${action}`)
-    }, 1000)
+  alertElement.textContent = text
+  alertElement.classList.add(`alert-${action}`)
+  // Remove Alert after 2 Seconds
+  setTimeout(() => {
+    alertElement.textContent = ''
+    alertElement.classList.remove(`alert-${action}`)
+  }, 2000)
 }
 
 // --------------- EVENT LISTENERS---------------
 
-form.addEventListener("submit", addItem);
+form.addEventListener('submit', addItem)
 
+// --------------- SET BACK TO DEFAULT  --------------
 
+const setBackToDefault = (id, value) => {
+  console.log('Set Back to Default')
+}
 // --------------- LOCAL STORAGE --------------
+
+const addToLocalStorage = (id, value) => {
+  console.log('Added to local storage')
+}
 
 // --------------- SETUP ITEMS ---------------
