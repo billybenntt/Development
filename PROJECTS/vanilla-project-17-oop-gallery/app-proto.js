bc898d2807e9124244cc12caa6e4d3a05be57b22
billbenntt@gapp.nthu.edu.tw <billbenntt@gapp.nthu.edu.tw>
2022-11-01 16:59:00 +0800

    update

--- PROJECTS/vanilla-project-17-oop-gallery/app-proto.js
+++ PROJECTS/vanilla-project-17-oop-gallery/app-proto.js
@@ -1,14 +1,76 @@
+// ------------  CONSTRUCTOR FUNCTION ------------
 
 function Gallery (element) {
 
-  this.section = element;
-  
+  /*Change NodeList to Array using SpreadOp (Map) */
+  this.list = [...element.querySelectorAll('.img')]
+  /* Select Modal Elements */
+  this.modal = getElement('.modal')
+  this.modalImg = getElement('.main-img')
+  this.imageName = getElement('.image-name')
+  this.modalImages = getElement('.modal-images')
+  this.closeBtn = getElement('.close-btn')
+  this.prevBtn = getElement('.prev-btn')
+  this.nextBtn = getElement('.next-btn')
+  /*Select the Whole Container */
+  this.container = element
+
+  /*Bind Function with Event to current Object */
+  this.eventModal = function (e) {
+    if (e.target.classList.contains('img')) {
+      this.openModal(e.target, this.list)
+    }
+  }.bind(this)
+
+  this.closeModal = this.closeModal.bind(this)
+  this.nextImage = this.nextImage.bind(this)
+  this.prevImage = this.prevImage.bind(this)
+
+  /* Open modal */
+  this.container.addEventListener('click', this.eventModal)
+  /* Close modal */
+  this.closeBtn.addEventListener('click', this.closeModal)
+  this.nextBtn.addEventListener('click', this.nextImage)
+  this.prevBtn.addEventListener('click', this.prevImage)
+
 }
 
+// ------------  PROTOTYPE FUNCTIONS ------------
 
+Gallery.prototype.openModal = function (selectedImage, list) {
+  /* Set Main Image */
+  this.setMainImage(selectedImage)
+  /* Inject Small Images */
+  this.modalImages.innerHTML = list.map(function (item) {
+    return `<img src="${item.src}" title="${item.title}" data-id="${item.dataset.id}" 
+     alt="${item.alt}" 
+    class="${selectedImage.dataset.id === item.dataset.id ? 'modal-img selected' : 'modal-img'}">`
+  }).join('')
+  /* Add Modal Open Class*/
+  this.modal.classList.add('open')
+}
 
+Gallery.prototype.closeModal = function () {
+  /* Add Modal Open Class*/
+  this.modal.classList.remove('open')
 
+  /*IMPORTANT - Remove Listeners so they do not pile up*/
+  this.closeBtn.removeEventListener('click', this.closeModal)
+  this.nextBtn.removeEventListener('click', this.nextImage)
+  this.prevBtn.removeEventListener('click', this.prevImage)
+}
 
+Gallery.prototype.nextImage = function () {
+}
+
+Gallery.prototype.prevImage = function () {
+}
+
+// Helper Function
+Gallery.prototype.setMainImage = function (selectedImage) {
+  this.modalImg.src = selectedImage.src
+  this.imageName.innerText = selectedImage.title
+}
 
 // ------------  HELPER FUNCTION ------------
 
@@ -21,3 +83,11 @@ function getElement (selection) {
     `Please check "${selection}" selector, no such element exists`
   )
 }
+
+// ------------  INSTANCES  ------------
+
+/* First Instance */
+const nature = new Gallery(getElement('.nature'))
+
+/* Second Instance */
+const city = new Gallery(getElement('.city'))
