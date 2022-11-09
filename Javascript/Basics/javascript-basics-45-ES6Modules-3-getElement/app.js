e040f9586a8a728ab8e44e26015641c24b2cb169
billbenntt@gapp.nthu.edu.tw <billbenntt@gapp.nthu.edu.tw>
2022-11-10 07:39:28 +0800

    update

---
+++ Javascript/Basics/javascript-basics-45-ES6Modules-3-getElement/app.js
@@ -0,0 +1,10 @@
+import getElement from './utils/getElement.js'
+
+
+// -------- SELECT ELEMENTS    ---------
+const container = getElement('.container')
+const btn = getElement('.btn')
+
+console.log(container)
+console.log(btn)
+
---
+++ Javascript/Basics/javascript-basics-45-ES6Modules-3-getElement/utils/getElement.js
@@ -0,0 +1,12 @@
+export default function (selection) {
+
+  const element = document.querySelector(selection)
+
+  if (element) {
+    return element
+  } else {
+    throw Error('You did not select element')
+  }
+
+}
+
