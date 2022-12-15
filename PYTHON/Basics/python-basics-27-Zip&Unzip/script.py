eb2134673aa81990f07b241f797abb423d6ab0b7
billbenntt@gapp.nthu.edu.tw <billbenntt@gapp.nthu.edu.tw>
2022-12-15 17:35:03 +0800

    update

--- PYTHON/Basics/python-basics-27-Zip&Unzip/script.py
+++ PYTHON/Basics/python-basics-27-Zip&Unzip/script.py
@@ -1,7 +1,28 @@
-# MODULES
-from platform import python_version as pv, system
+# ZIP & UNZIP
+
+# Base Values
+numbers = [1, 2, 3, 4]
+number_string = '1234'
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+names = ['John', 'Eric', 'Michael', 'Graham', 'Joe']
+
+# Zip to Datatype
+combo_one = list(zip(numbers, letters))
+combo_two = dict(zip(number_string, letters))
+combo_three = dict(zip(names, letters))
+combo_four = list(zip(names, letters, numbers))
+
+# Unzip from Tuple
+value1, value2, value3 = zip(*combo_four)
 
 
-print(pv())
-print(system())
+# Zipped Values
+print(combo_one)
+print(combo_two)
+print(combo_three)
+print(combo_four)
 
+# Unzipped Values
+print(value1)
+print(value2)
+print(value3)
