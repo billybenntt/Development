38c8a81d69e4eeb07af9b59d73e22b00d169f8c4
billbenntt@gapp.nthu.edu.tw <billbenntt@gapp.nthu.edu.tw>
2023-01-05 13:52:59 +0800

    update python notes

--- PYTHON/Basics/python-basics-28-LambdaFunction/script.py
+++ PYTHON/Basics/python-basics-28-LambdaFunction/script.py
@@ -1,2 +1,10 @@
 # =========== LAMBDA FUNCTIONS ===========
 
+# Anonymous function 1 argument
+anon_fun_1 = lambda a: a + 10
+
+# Anonymous function 2 arguments
+anon_fun_2 = lambda a, b: a * b
+
+print(anon_fun_1(10))
+print(anon_fun_2(10, 15))
