5b82ec21a11f1fe13ef6cc793a7625d979cb6f53
billbenntt@gapp.nthu.edu.tw <billbenntt@gapp.nthu.edu.tw>
2023-01-07 13:57:57 +0800

    complete tea station project

--- PROJECTS/html-project-8-TeaStation/app.js
+++ PROJECTS/html-project-8-TeaStation/app.js
@@ -1,22 +1,29 @@
 //setup date
 const date = document.getElementById('date');
 if (date) {
-  date.innerHTML = new Date().getFullYear();
+  date.innerText = new Date().getFullYear();
 }
-// setup nav
+
+
+// -------------- SELECT ELEMENTS --------------
 const navBtn = document.getElementById('nav-btn');
 const navbar = document.getElementById('navbar');
 const navClose = document.getElementById('nav-close');
-// show nav
+
+
+
+// -------------- EVENT LISTENERS  --------------
+
+/* Show Navbar  */
 if (navBtn) {
   navBtn.addEventListener('click', () => {
-    navbar.classList.add('showNav');
+    navbar.classList.add('show');
   });
 }
 
+/* Hide  Navbar  */
 if (navClose) {
   navClose.addEventListener('click', () => {
-    navbar.classList.remove('showNav');
+    navbar.classList.remove('show');
   });
 }
-// close nav
